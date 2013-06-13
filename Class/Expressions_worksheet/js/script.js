6a3a5678d6e6b75bef9d9f18c1fadfea909e78a6
wnorton1911 <wnorton1911@gmail.com>
2013-06-13 18:16:02 -0400

    expressions worksheet problem 1 final

---
+++ Class/Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+//William Norton 6-13-2013 Expression Worksheet
+
+var sparkysAge = 7;  //variable for sparkys age
+var humanAge = prompt("Enter number of years Sparkys been alive in human years", 8);   //variable for human years
+
+var sparkysRealAge = humanAge * sparkysAge; //calculation for sparkys real age
+
+console.log("Sparkys been alive for " + sparkysRealAge + " " + "dog years"); // print results to console
+
+
