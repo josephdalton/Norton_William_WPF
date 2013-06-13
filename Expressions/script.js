1bdcbb69d3897b92522988762b4cdab9ec69c42f
wnorton1911 <wnorton1911@gmail.com>
2013-06-13 00:54:49 -0400

    expressions. screencast on concatenating

--- Expressions/script.js
+++ Expressions/script.js
@@ -1,8 +1,8 @@
-// expressions with arrays
+// expressions with concantenating strings
 
-var orangeBins = [234, 567, 883];
+var a = 6;
+var b = 7;
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-
-console.log(total);
+var result = a + b;
+console.log(result);
 
