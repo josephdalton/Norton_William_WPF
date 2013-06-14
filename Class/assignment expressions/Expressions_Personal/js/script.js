58d31d8e1413c3ff45368c6b9a0eb2b4c4df4ea6
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:15:38 -0400

    comments added

--- Class/assignment expressions/Expressions_Personal/js/script.js
+++ Class/assignment expressions/Expressions_Personal/js/script.js
@@ -1,6 +1,6 @@
 // William Norton June 14, 2013 Assignment: Expressions
 
-// Personal Calculator 
+// Personal Calculator for status of dog food
 
 var poundsOfFood = prompt("How many pounds of food for the dogs do we have?"); //prompt user for amount of food on hand
 var dogKatie = prompt("How many pounds of food does Katie eat in a week?"); // prompt for amount of dog food Katie eats
