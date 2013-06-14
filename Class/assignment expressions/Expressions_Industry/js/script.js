e2023c4fe31aaba13689236d91a39f9b88b5483d
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:52:33 -0400

    comments for prompting of array

--- Class/assignment expressions/Expressions_Industry/js/script.js
+++ Class/assignment expressions/Expressions_Industry/js/script.js
@@ -4,7 +4,7 @@
 
 var remainPaint = new Array(3); // Starts new array
 	remainPaint[0] = prompt("How many gallons of paint left in can 1?"); //prompt user for array value
-	remainPaint[1] = prompt("How many gallons of paint left in can 2?");
+	remainPaint[1] = prompt("How many gallons of paint left in can 2?"); //prompt user for array value
 	remainPaint[2] = prompt("How many gallons of paint left in can 3?");
 	
 var bigJob = prompt("How many gallons of paint is needed for this job?");
