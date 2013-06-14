6e90d601c7f9fb94a9987cfea3f8f5fe64a6246e
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:08:38 -0400

    calculating variable created

--- Class/assignment expressions/Expressions_Personal/js/script.js
+++ Class/assignment expressions/Expressions_Personal/js/script.js
@@ -6,7 +6,7 @@ var poundsOfFood = prompt("How many pounds of food for the dogs do we have?"); /
 var dogKatie = prompt("How many pounds of food does Katie eat in a week?");
 var dogDash = prompt("How many pounds of food does Dash eat in a week?");
 
-
+var enoughFood = parseInt(dogKatie) + parseInt(dogDash);
 
 
 
