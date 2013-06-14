55b45f1963abbbe7ec367be8bfe045dc6ab109a2
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:49:56 -0400

    final array prompt

--- Class/assignment expressions/Expressions_Industry/js/script.js
+++ Class/assignment expressions/Expressions_Industry/js/script.js
@@ -5,4 +5,5 @@
 var remainPaint = new Array(3);
 	remainPaint[0] = prompt("How many gallons of paint left in can 1?");
 	remainPaint[1] = prompt("How many gallons of paint left in can 2?");
+	remainPaint[2] = prompt("How many gallons of paint left in can 3?");
 	
