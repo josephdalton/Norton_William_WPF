7c55b9bd6159620c79cbb92ef1697a00e7f29283
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:06:16 -0400

    first variable made

--- Class/assignment expressions/Expressions_Personal/js/script.js
+++ Class/assignment expressions/Expressions_Personal/js/script.js
@@ -1,6 +1,16 @@
 // William Norton June 14, 2013 Assignment: Expressions
 
-// Personal Calculator
+// Personal Calculator 
+
+var poundsOfFood = prompt("How many pounds of food for the dogs do we have?");
+var dogKatie = prompt("How many pounds of food does Katie eat in a week?");
+var dogDash = prompt("How many pounds of food does Dash eat in a week?");
+
+var enoughFood = parseInt(dogKatie) + parseInt(dogDash);
+ 
+
+console.log(enoughFood);
+
 
 
 
