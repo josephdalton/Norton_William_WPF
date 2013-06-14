d1f7667ebf294e205797ae76e42b2a61751903ea
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:54:00 -0400

    comments for parsing of array for addition

--- Class/assignment expressions/Expressions_Industry/js/script.js
+++ Class/assignment expressions/Expressions_Industry/js/script.js
@@ -9,6 +9,6 @@ var remainPaint = new Array(3); // Starts new array
 	
 var bigJob = prompt("How many gallons of paint is needed for this job?"); //prompt user for amount of paint needed
 
-var totalLeft = parseInt(remainPaint[0]) + parseInt(remainPaint[1]) + parseInt(remainPaint[2]);
+var totalLeft = parseInt(remainPaint[0]) + parseInt(remainPaint[1]) + parseInt(remainPaint[2]); //parsing of the array for addition of values
 
 console.log("We need" + " " + bigJob + " " + "gallons of paint for this job. We have" + " " + totalLeft + " " + "gallons left over.");
