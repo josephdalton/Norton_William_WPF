78a1575fc445e1cf916ac48af3ec1a9aa190a926
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 19:21:25 -0400

    comments

--- Class/assignment expressions/Expressions_Wacky/js/script.js
+++ Class/assignment expressions/Expressions_Wacky/js/script.js
@@ -5,7 +5,7 @@
 var weeklyMileage = new Array(4); //setting up variable array
 	weeklyMileage[0] = prompt("Gas mileage for week 1?"); //prompting for array
 	weeklyMileage[1] = prompt("Gas mileage for week 2?"); //prompting for array
-	weeklyMileage[2] = prompt("Gas mileage for week 3?");
+	weeklyMileage[2] = prompt("Gas mileage for week 3?"); //prompting for array
 	weeklyMileage[3] = prompt("Gas mileage for week 4?");
 	
 var monthMileage = parseInt(weeklyMileage[0]) + parseInt(weeklyMileage[1]) + parseInt(weeklyMileage[2]) + parseInt(weeklyMileage[3]);
