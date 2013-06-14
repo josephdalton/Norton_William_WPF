fd07f1ad763aa106171f562d987da5b2f9bf7b84
wnorton1911 <wnorton1911@gmail.com>
2013-06-14 18:08:03 -0400

    third variable added

--- Class/assignment expressions/Expressions_Personal/js/script.js
+++ Class/assignment expressions/Expressions_Personal/js/script.js
@@ -4,6 +4,8 @@
 
 var poundsOfFood = prompt("How many pounds of food for the dogs do we have?"); //prompt user for amount of food on hand
 var dogKatie = prompt("How many pounds of food does Katie eat in a week?");
+var dogDash = prompt("How many pounds of food does Dash eat in a week?");
+
 
 
 
