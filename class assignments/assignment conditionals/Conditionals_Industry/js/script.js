91c11e4e9f0f19924c66948e5c9a3415c819e9ed
wnorton1911 <wnorton1911@gmail.com>
2013-06-20 19:39:00 -0400

    comments added

--- class assignments/assignment conditionals/Conditionals_Industry/js/script.js
+++ class assignments/assignment conditionals/Conditionals_Industry/js/script.js
@@ -16,7 +16,7 @@ if (part[0] === "yes"){ //if statement for taking prompt from array[0]
 	console.log(painted); //print variable painted to console.log
 }
 if (part[1] === "yes"){ //if statement for array[1] prompt
-	console.log(welded);
+	console.log(welded); //prints variable welded to console.log
 }else{
 	console.log(painted);
 }
