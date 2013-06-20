76346d165ee971abf60d15b6ebdcede06a71caab
wnorton1911 <wnorton1911@gmail.com>
2013-06-20 19:20:46 -0400

    prompt for "part" array index 0

--- class assignments/assignment conditionals/Conditionals_Industry/js/script.js
+++ class assignments/assignment conditionals/Conditionals_Industry/js/script.js
@@ -3,4 +3,7 @@
 //Conditionals for Industry
 
 var part = new Array(); //New array for part
+	part[0] = prompt("Does this part get welded before getting painted?"); //prompt for new array index 0
+
+
 
