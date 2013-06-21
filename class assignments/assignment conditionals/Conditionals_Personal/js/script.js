85374820abbad37c7aa50bfab2b1a31ec5a5b0ee
wnorton1911 <wnorton1911@gmail.com>
2013-06-20 20:56:40 -0400

    comments added for variable over18

--- class assignments/assignment conditionals/Conditionals_Personal/js/script.js
+++ class assignments/assignment conditionals/Conditionals_Personal/js/script.js
@@ -2,8 +2,8 @@
 
 //Conditionals for Personal
 
-var over18 = prompt("Are you 18 or older?");
-var kidsAge = prompt("What is the age of the child?");
+var over18 = prompt("Are you 18 or older?"); //variable for over18
+var kidsAge = prompt("What is the age of the child?"); 
 var underAgeBed = "9 o'clock.";
 var teenAgeBed = "10 o'clock.";
 var over18Note = "Better use good judgement on your bedtime.";
