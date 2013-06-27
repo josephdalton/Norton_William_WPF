35ad63dbd1d749a420870a014193ec0697825596
wnorton1911 <wnorton1911@gmail.com>
2013-06-27 17:48:41 -0400

    comment for console.log

--- class assignments/assignment functions/Functions_Wacky/js/script.js
+++ class assignments/assignment functions/Functions_Wacky/js/script.js
@@ -21,7 +21,7 @@ var totalAmount = amountOfAllowance(); //var totalamount
 if (age <= 12){	 //if statement 
 	var newAmount = totalAmount - jobs * 2; //variable new amount math
 	var fullyVested = 12 - age; //variable for math
-	console.log("Good job on your work but your not fully vested so you get " + newAmount + " dollars." + " You have " + fullyVested + " years till you get the full amount");
+	console.log("Good job on your work but your not fully vested so you get " + newAmount + " dollars." + " You have " + fullyVested + " years till you get the full amount"); //print this to the console.log
 }else{	
 	console.log("Good job, you get " + totalAmount + " dollars.");
 }
