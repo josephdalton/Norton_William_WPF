0823037a320d3424355dfe076b0c7183268a7ee7
wnorton1911 <wnorton1911@gmail.com>
2013-06-27 17:45:32 -0400

    comment on function

--- class assignments/assignment functions/Functions_Wacky/js/script.js
+++ class assignments/assignment functions/Functions_Wacky/js/script.js
@@ -10,7 +10,7 @@ var age = prompt("How old are you?"); //prompts for age variable
 var payRate = 5; //payrate is 5 dollars
 var jobs = prompt("How many jobs did you do?"); //number of jobs prompts
 
-var amountOfAllowance = function(){
+var amountOfAllowance = function(){ //function variable amountofallowance
 	var totalAllowance = payRate * jobs;
 	return totalAllowance;
 }
