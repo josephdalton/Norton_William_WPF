572d368e86297a5a512bba10969f3f6689fcdfa7
wnorton1911 <wnorton1911@gmail.com>
2013-06-26 21:25:45 -0400

    comment for radius to diameter

--- class assignments/functions worksheet/js/script.js
+++ class assignments/functions worksheet/js/script.js
@@ -10,7 +10,7 @@ var circum = circumMath(); //variable circum defined as a function
 function circumMath(){     //function circummath started
 	var rad = prompt("What is the radius?"); //variable radius prompts for radius 
 	var radToDia = rad * 2; //variable radius to diameter with math
-	var diaToCir = radToDia * 3.14;
+	var diaToCir = radToDia * 3.14; //diameter to circumference math
 	return diaToCir;
 }
 console.log("The circumference of the circle is " + circum); 
