f486abfd13b357ed1fc242b347c836c2c7442bad
wnorton1911 <wnorton1911@gmail.com>
2013-06-26 23:28:11 -0400

    set up directory structure, index.html and script.js files

---
+++ class assignments/assignment functions/Functions_Wacky/js/script.js
@@ -0,0 +1,17 @@
+/* 
+William Norton
+Assignment: Functions
+June 26,2013
+*/
+
+//Functions_Wacky
+
+
+var age = prompt("How old are you?");
+var allowance = 10;
+var jobs = 3;
+var owe = true;
+
+
+function allowance(){
+	
