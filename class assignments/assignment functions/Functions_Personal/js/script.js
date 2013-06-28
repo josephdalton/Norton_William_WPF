c86b3ddb70e2a2b6a74f13c4280f8abf119876e3
wnorton1911 <wnorton1911@gmail.com>
2013-06-27 21:49:38 -0400

    variable totalamount function

--- class assignments/assignment functions/Functions_Personal/js/script.js
+++ class assignments/assignment functions/Functions_Personal/js/script.js
@@ -10,3 +10,5 @@ var name = prompt("What is your name?");
 var drinkToday = prompt("How much Red Bull have you had today in OZ?");
 var safeAmount = 40;
 
+var totalAmount = function caffeineBuzz(){
+	
